e817606e6277e617fbd9b8de7e32a5cf521d0ae6
Mohamad Sleiman Haidar <mhaidar@myki.com>
2023-01-07 13:02:05 +0300

    add case for when uploading voice recording in store upload

--- media.js
+++ media.js
@@ -19,7 +19,11 @@ async function media(index, params) {
   switch (index) {
     case "UPLOAD_IMAGE": {
       try {
-        const file = await storeUpload(params.image, params.readStream);
+        const file = await storeUpload(
+          "UPLOAD_IMAGE",
+          params.image,
+          params.readStream
+        );
         return file;
       } catch (e) {
         console.log("error:", e);
@@ -29,19 +33,25 @@ async function media(index, params) {
 
     case "UPLOAD_AUDIO_RECORDING": {
       try {
-        console.log("upload audio recording index:", index);
-        console.log("upload audio recording params:", params);
-        console.log("upload audio recording buffer:", params.file.buffer.data);
-        let filename = Math.floor(Date.now() / 1000) + ".ogg";
-
-        let binary = Buffer.from(params.file.buffer.data, "base64");
-
-        fs.writeFileSync(
-          path.join(__dirname + "/public/audio/") + filename,
-          binary
+        const file = await storeUpload(
+          "UPLOAD_AUDIO",
+          params.file,
+          params.readStream
         );
-
-        return process.env.URL + "/audio/" + filename;
+        return file;
+        // console.log("upload audio recording index:", index);
+        // console.log("upload audio recording params:", params);
+        // console.log("upload audio recording buffer:", params.file.buffer.data);
+        // let filename = Math.floor(Date.now() / 1000) + ".ogg";
+        //
+        // let binary = Buffer.from(params.file.buffer.data, "base64");
+        //
+        // fs.writeFileSync(
+        //   path.join(__dirname + "/public/audio/") + filename,
+        //   binary
+        // );
+        //
+        // return process.env.URL + "/audio/" + filename;
       } catch (e) {
         console.log("error:", e);
         return false;
--- storeUpload.js
+++ storeUpload.js
@@ -7,16 +7,37 @@ const Jimp = require("jimp");
 
 // const UPLOAD_DIRECTORY_URL = require('../../../config/UPLOAD_DIRECTORY_URL');
 const sharp = require("sharp");
-const storeUpload = async (image, readStream) => {
+const storeUpload = async (task, file, readStream) => {
   try {
     const buffer = Buffer.from(readStream.data, "base64");
 
-    await sharp(buffer).toFile(
-      path.join(__dirname + "/public/images/") + image.filename,
-      (err, info) => {}
-    );
+    if (task === "UPLOAD_IMAGE") {
+      await sharp(buffer).toFile(
+        path.join(__dirname + "/public/images/") + file.filename,
+        (err, info) => {}
+      );
 
-    return process.env.URL + "/images/" + image.filename;
+      return process.env.URL + "/images/" + file.filename;
+    } else if (task === "UPLOAD_AUDIO") {
+      // await sharp(buffer).toFile(
+      //   path.join(__dirname + "/public/audio/") + image.filename,
+      //   (err, info) => {}
+      // );
+      console.log("file:", file);
+      console.log("upload audio recording buffer:", readStream);
+      let filename = Math.floor(Date.now() / 1000) + ".ogg";
+
+      let binary = Buffer.from(readStream.data, "base64");
+
+      fs.writeFileSync(
+        path.join(__dirname + "/public/audio/") + filename,
+        binary
+      );
+
+      return process.env.URL + "/audio/" + filename;
+
+      // return process.env.URL + "/audio/" + image.filename;
+    }
   } catch (e) {
     console.log("error:", e);
   }
