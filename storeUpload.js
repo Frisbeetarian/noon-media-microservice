2e96118690b927d9763e88f0d692d5390e0ea35a
Muhammad Sulayman Haydar <mohamad.sleimanhaidar@gmail.com>
2024-02-08 23:12:52 +0200

    fix getting extension of voice notes

--- storeUpload.js
+++ storeUpload.js
@@ -1,13 +1,9 @@
 const fs = require("fs");
-const jsonfn = require("json-fn");
-const shortId = require("shortid");
 const path = require("path");
-const Jimp = require("jimp");
 const { v4: uuidv4 } = require("uuid");
-// const dotenv = require("dotenv-safe").config({ silent: true });
-
-// const UPLOAD_DIRECTORY_URL = require('../../../config/UPLOAD_DIRECTORY_URL');
+const mime = require("mime-types");
 const sharp = require("sharp");
+
 const baseURL = process.env.URL || "http://localhost:4060";
 
 const storeUpload = async (task, file) => {
@@ -27,10 +23,9 @@ const storeUpload = async (task, file) => {
       const actualBuffer = Buffer.isBuffer(file.buffer)
         ? file.buffer
         : Buffer.from(file.buffer.data);
-      const originalFilename = file.filename;
-      const extension = path.extname(originalFilename);
+      const mimeType = file.mimetype;
+      const extension = mime.extension(mimeType) || ".ogg";
       const uuidFilename = `${uuidv4()}${extension}`;
-      // const filename = `${Math.floor(Date.now() / 1000)}.ogg`;
       const outputPath = path.join(__dirname, "public", "audio", uuidFilename);
       fs.writeFileSync(outputPath, actualBuffer);
       return `${baseURL}/audio/${uuidFilename}`;
@@ -39,32 +34,6 @@ const storeUpload = async (task, file) => {
     console.log("error:", e);
     throw e;
   }
-
-  // try {
-  //   const buffer = Buffer.from(readStream.data, "base64");
-  //
-  //   if (task === "UPLOAD_IMAGE") {
-  //     await sharp(buffer).toFile(
-  //       path.join(__dirname + "/public/images/") + file.filename,
-  //       (err, info) => {}
-  //     );
-  //
-  //     return process.env.URL + "/images/" + file.filename;
-  //   } else if (task === "UPLOAD_AUDIO") {
-  //     let filename = Math.floor(Date.now() / 1000) + ".ogg";
-  //
-  //     let binary = Buffer.from(readStream.data, "base64");
-  //
-  //     fs.writeFileSync(
-  //       path.join(__dirname + "/public/audio/") + filename,
-  //       binary
-  //     );
-  //
-  //     return process.env.URL + "/audio/" + filename;
-  //   }
-  // } catch (e) {
-  //   console.log("error:", e);
-  // }
 };
 
 module.exports = storeUpload;
